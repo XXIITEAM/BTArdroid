a7c66f96e95c2b7fc51c0dbee67a02b8bcfaf13b
XXIITEAM <xxiiteam@gmail.com>
2018-12-21 18:28:22 +0100

    arduino activity

--- app/src/main/java/com/ip/jmc/btardroid/ArduinoDroid.java
+++ app/src/main/java/com/ip/jmc/btardroid/ArduinoDroid.java
@@ -1,6 +1,5 @@
 package com.ip.jmc.btardroid;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,15 +11,9 @@ public class ArduinoDroid extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arduino_droid);
-        //  bt1 = findViewById(R.id.bt1);
-        // bt2 = findViewById(R.id.bt2);
-        // bt3 = findViewById(R.id.bt3);
-        // bt4 = findViewById(R.id.bt4);
-        // bt5 = findViewById(R.id.bt5);
-        //bt6 = findViewById(R.id.bt6);
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        //Intent intent = getIntent();
+        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
         //TextView textView = findViewById(R.id.TV01);
@@ -28,33 +21,68 @@ public class ArduinoDroid extends MainActivity {
 
     }
 
-    public void bt2Click(View v) {
+    public void btn1Click(View v) {
         deviceInterface.sendMessage("2");
         msgSent = "2";
     }
 
-    public void bt3Click(View v) {
+    public void btn2Click(View v) {
+        deviceInterface.sendMessage("2");
+        msgSent = "2";
+    }
+
+    public void btn3Click(View v) {
         deviceInterface.sendMessage("3");
         msgSent = "3";
     }
 
-    public void bt4Click(View v) {
+    public void btn4Click(View v) {
         deviceInterface.sendMessage("4");
         msgSent = "4";
     }
 
-    public void bt5Click(View v) {
-        //deviceInterface.sendMessage("5");
-        //msgSent = "5";
-        Intent intent = new Intent(this, ArduinoDroid.class);
-        //TextView editText = findViewById(R.id.editText);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    public void btn5Click(View v) {
+        deviceInterface.sendMessage("4");
+        msgSent = "4";
 
     }
 
-    public void bt6Click(View v) {
+    public void btn6Click(View v) {
+        deviceInterface.sendMessage("6");
+        msgSent = "6";
+    }
+
+    public void btn7Click(View v) {
+        deviceInterface.sendMessage("6");
+        msgSent = "6";
+    }
+
+    public void btn8Click(View v) {
+        deviceInterface.sendMessage("6");
+        msgSent = "6";
+    }
+
+    public void btnHautClick(View v) {
+        deviceInterface.sendMessage("6");
+        msgSent = "6";
+    }
+
+    public void btnBasClick(View v) {
+        deviceInterface.sendMessage("6");
+        msgSent = "6";
+    }
+
+    public void btnGaucheClick(View v) {
+        deviceInterface.sendMessage("6");
+        msgSent = "6";
+    }
+
+    public void btnDroiteClick(View v) {
+        deviceInterface.sendMessage("6");
+        msgSent = "6";
+    }
+
+    public void btnAccelClick(View v) {
         deviceInterface.sendMessage("6");
         msgSent = "6";
     }
